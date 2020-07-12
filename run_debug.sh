753d62ed5c27bf3e60f672f6dac088abe03100b7
Anton Miroshnichenko <anton.miroshnichenko@globallogic.com>
2020-07-12 23:23:36 +0300

    Debug script added

--- run_debug.sh
+++ run_debug.sh
@@ -2,4 +2,4 @@
 read -sp 'Password: ' userpassw
 sshpass -p $userpassw scp cmake-build-debug/src/airc_dev.elf airc@opi:firmware
 sshpass -p $userpassw ssh airc@opi "cd firmware && ./flash.sh airc_dev.elf"
-sshpass -p $userpassw ssh airc@opi -L 3333:localhost:3333 openocd -f firmware/openocd_commands.cfg &
+sshpass -p $userpassw ssh airc@opi -L 3333:localhost:3333 openocd -f firmware/openocd_commands.cfg
