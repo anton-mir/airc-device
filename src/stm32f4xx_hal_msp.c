a82190f0ba97ef86cd07ef7928abf164e1768ef3
creci <creci@mail.ru>
2020-09-02 13:47:06 +0300

    Fix multiple definition of HAL_MspInit

--- src/stm32f4xx_hal_msp.c
+++ src/stm32f4xx_hal_msp.c
@@ -61,21 +61,6 @@
 /**
   * Initializes the Global MSP.
   */
-void HAL_MspInit(void)
-{
-  /* USER CODE BEGIN MspInit 0 */
-
-  /* USER CODE END MspInit 0 */
-
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_RCC_PWR_CLK_ENABLE();
-
-  /* System interrupt init*/
-
-  /* USER CODE BEGIN MspInit 1 */
-
-  /* USER CODE END MspInit 1 */
-}
 
 /**
 * @brief SPI MSP Initialization
