e44f4b625f63a006d3f1685315393b16a4c28685
creci <creci@mail.ru>
2020-09-27 13:21:15 +0300

    Fixed error with cfg.

--- src/config_board.c
+++ src/config_board.c
@@ -58,24 +58,24 @@ void WriteConfig(boxConfig_S cfg)
     cfg_crc.CRC16_CCITT=CRC16_CCITT(&cfg,sizeof(boxConfig_S));
     WriteToStatusRegister(0);
     ClearSector(ADDRESS_CFG_START);
-    WriteDataArrayWithAAI(ADDRESS_CFG_START,((uint8_t*)&cfg_crc),sizeof(boxConfig_CRC16_CCITT_S));
+    WriteDataArrayWithAAI(ADDRESS_CFG_START,((uint8_t*)&cfg_crc),(sizeof(boxConfig_S)+sizeof(unsigned short)));
     WriteToStatusRegister(0);
     ClearSector(ADDRESS_CFG_START_RESERV);
-    WriteDataArrayWithAAI(ADDRESS_CFG_START_RESERV,((uint8_t*)&cfg_crc),sizeof(boxConfig_CRC16_CCITT_S));
+    WriteDataArrayWithAAI(ADDRESS_CFG_START_RESERV,((uint8_t*)&cfg_crc),(sizeof(boxConfig_S)+sizeof(unsigned short)));
     return;
 }
 
 void ReadConfig(boxConfig_S* cfg)
 {
     boxConfig_CRC16_CCITT_S cfg_crc;
-    ReadDataArrayFromAddress(ADDRESS_CFG_START,((uint8_t*)cfg_crc),sizeof(boxConfig_CRC16_CCITT_S));
+    ReadDataArrayFromAddress(ADDRESS_CFG_START,((uint8_t*)cfg_crc),(sizeof(boxConfig_S)+sizeof(unsigned short)));
     if(cfg_crc.CRC16_CCITT==CRC16_CCITT(&(cfg_crc.cfg),sizeof(boxConfig_S)))
     {
         *(cfg)=cfg_crc.cfg;
         return;
     }else
     {
-        ReadDataArrayFromAddress(ADDRESS_CFG_START_RESERV,((uint8_t*)cfg_crc),sizeof(boxConfig_CRC16_CCITT_S));
+        ReadDataArrayFromAddress(ADDRESS_CFG_START_RESERV,((uint8_t*)cfg_crc),(sizeof(boxConfig_S)+sizeof(unsigned short)));
         if(cfg_crc.CRC16_CCITT==CRC16_CCITT(&(cfg_crc.cfg),sizeof(boxConfig_S)))
         {
             *(cfg)=cfg_crc.cfg;
