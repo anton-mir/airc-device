8be7d4d4c6a090298fca32fd852e3ded9ac48c48
creci <creci@mail.ru>
2020-10-07 11:56:19 +0300

    Updated data structure.

--- src/data_collector.c
+++ src/data_collector.c
@@ -14,8 +14,8 @@ void data_collector(void *pvParameters)
     xEventGroupWaitBits(
             eg_task_started,
             (EG_INIT_STARTED | EG_ETHERIF_IN_STARTED | EG_LINK_STATE_STARTED |
-            EG_DHCP_FSM_STARTED | EG_ETH_SERVER_STARTED | EG_ETH_SENDER_STARTED |
-            EG_ANALOG_TEMP_STARTED | EG_UART_SENSORS_STARTED),
+             EG_DHCP_FSM_STARTED | EG_ETH_SERVER_STARTED | EG_ETH_SENDER_STARTED |
+             EG_ANALOG_TEMP_STARTED | EG_UART_SENSORS_STARTED),
             pdFALSE,
             pdTRUE,
             portMAX_DELAY);
